603c1cf15ef6d02146eca8c3d878de5fde59b96b
gabriel <levitarmouse@gmail.com>
2017-03-23 14:02:04 -0300

    analisis, integración componentes

--- Response.php
+++ Response.php
@@ -38,6 +38,8 @@ class Response
     const USER_DOES_NOT_EXIST = 'USER_DOES_NOT_EXIST';
     const ACCESS_DENIED = 'ACCESS_DENIED';
     const ACCESS_ORG_DENIED = 'ACCESS_ORG_DENIED';
+    const DB_ACCESS_DENIED = 'DB_ACCESS_DENIED';
+    const DB_ACCESS_FAILED = 'DB_ACCESS_FAILED';
     const PASSWORD_FORMAT_ERROR = 'PASSWORD_FORMAT_ERROR';
     const PASSWORD_VERIFICATION_ERROR = 'PASSWORD_VERIFICATION_ERROR';
     const VALID_CSRF_TOKEN_IS_REQUIRED = 'VALID_CSRF_TOKEN_IS_REQUIRED';
@@ -84,6 +86,10 @@ class Response
             self::NICK_NAME_OR_PASSTOKEN_EMPTY => array('id' => 110, 'description' => 'Usuario o Clave invalidos'),
             self::ACCESS_DENIED                => array('id' => 114, 'description' => 'El acceso está restringido para sus credenciales de acceso'),
             self::ACCESS_ORG_DENIED            => array('id' => 115, 'description' => 'El acceso está restringido por Inhabilitación en jerarquía'),
+            
+            self::DB_ACCESS_DENIED            => array('id' => 118, 'description' => 'El acceso a la DB no está habilitado para las credenciales indicadas'),
+            self::DB_ACCESS_FAILED            => array('id' => 119, 'description' => 'La conexión hacia la DB no pudo establecerse'),
+            
             self::ADMIN_ACCESS_INVALID         => array('id' => 116, 'description' => 'Autenticación con provilegios inválida'),
             self::INVALID_LONG_PARAMS          => array('id' => 117, 'description' => 'Los parámetros exceden la longitud permitida'),
 
